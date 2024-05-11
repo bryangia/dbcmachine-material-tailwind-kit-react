2713bd227761348c9560ecc726c197e36f15755c
bryangia <bryangia12345@gmail.com>
2024-05-11 09:24:06 -0500

    Deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/',
+  base: '/dbcmachine-material-tailwind-kit-react',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
